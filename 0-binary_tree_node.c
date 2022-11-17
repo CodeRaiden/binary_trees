3e9895c5000b4ce7eaa62cc80cc5e3938b76540a
CodeRaiden <hadoken10@yahoo.com>
2022-11-17 01:17:26 -0800

    added 0-binary_tree_node.c

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - created a binary tree node
+ * @parent: parent node
+ * @value: value of the node
+ * Return: Null on failure or ptr to new node
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value){
+	binary_tree_t *newNode;
+	newNode = malloc(sizeof(binary_tree_t));
+	if(newNode == NULL)
+		return NULL;
+	newNode = perent;
+	newNode->n = value;
+	newNode->left = newNode->right = NULL;
+	return (newNode);
+}
