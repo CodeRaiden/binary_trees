619e518c2576a262ee76d6a39684c2f40a6c13f4
CodeRaiden <hadoken10@yahoo.com>
2022-11-17 01:34:24 -0800

    added 0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -7,13 +7,16 @@
  * Return: Null on failure or ptr to new node
  */
 
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value){
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
 	binary_tree_t *newNode;
+
 	newNode = malloc(sizeof(binary_tree_t));
-	if(newNode == NULL)
-		return NULL;
+	if (newNode == NULL)
+		return (NULL);
 	newNode = perent;
 	newNode->n = value;
 	newNode->left = newNode->right = NULL;
 	return (newNode);
 }
+
