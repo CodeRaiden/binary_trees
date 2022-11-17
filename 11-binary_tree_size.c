6bcca3ebae658d1bf4f06809e21ef894ad07218e
CodeRaiden <hadoken10@yahoo.com>
2022-11-17 10:49:03 -0800

    added 11-binary_tree_size.c

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -7,14 +7,12 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	int num;
-
 	size_t le, re;
 
 	if (tree == NULL)
 		return (0);
 	le = binary_tree_size(tree->left) + 1;
 	re = binary_tree_size(tree->right) + 1;
-	return (re + le);
+	return (re + le + 1);
 }
 
