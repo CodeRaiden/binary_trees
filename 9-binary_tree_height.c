ccb166756c842fb68be3a751c8fc0d2f0e16a7a7
CodeRaiden <hadoken10@yahoo.com>
2022-11-17 10:05:31 -0800

    added 9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,9 +9,9 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t le, re;
+	size_t re, le;
 
-	if (tree == NULL || (tree->left && tree->right == NULL))
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
 		return (0);
 	le = binary_tree_height(tree->left) + 1;
 	re = binary_tree_height(tree->right) + 1;
